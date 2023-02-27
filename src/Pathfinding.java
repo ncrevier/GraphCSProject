f9b9cecb40763abf95c7ce381ad0e73ca8f4096e
ncrevier <ncrevier@exeter.edu>
2023-02-27 16:28:09 -0500

    adding Pnode changing Pathfinding.java

--- src/Pathfinding.java
+++ src/Pathfinding.java
@@ -3,33 +3,26 @@ public class Pathfinding {
     private Pnode start, finish;
     private Graph graph;
     Pathfinding(int s, int f, Graph g) {
-        start = new Pnode(s);
-        finish = new Pnode(f);
         graph = g;
+        start = new Pnode(graph.getConnections(s));
+        finish = new Pnode(graph.getConnections(f));
     }
     public int[] findPath() {
-        ArrayList<Pnode> open = new ArrayList<>();
-        ArrayList<Pnode> closed = new ArrayList<>();
-
-        open.add(start);
-
-        while(open.size() > 0) {
-            Pnode curr = open.get(0);
-            for (int i = 1; i<open.size(); i++) {
-                if (open.get(i).getFCost() < curr.getFCost() || open.get(i).getFCost() == curr.getFCost()
-                                                                && open.get(i).getHCost() < curr.getHCost()) {
-                    curr = open.get(i);
-                }
-            }
-
-            open.remove(curr);
-            closed.add(curr);
-
-            if (curr.getVal() == finish.getVal()) {
-                // done
-            }
-
+        Pnode[] coolerGraph = new Pnode[graph.getSize()];
+        for (int i = 0; i<graph.getSize(); i++) {
+            coolerGraph[i] = new Pnode(graph.getConnections(i));
         }
+        start.setDistance(0);
+        List<Pnode> closed = new ArrayList<>();
+        PriorityQueue<Pnode> open = new PriorityQueue<>(Collections.singleton(start));
+
+
+        for (int i = 0; i<coolerGraph.length; i++) {
+            if (i != 0) {
+                coolerGraph[i].setDistance(start.getAdjacentNodes()[i]);
+            }
+        }
+
     }
 
 }
