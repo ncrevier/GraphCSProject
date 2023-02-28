a1da2c14db81f6d025128707075c132f04dcfd67
NutellaNaf <nafeesabdullah122@gmail.com>
2023-02-28 15:24:05 -0500

    pathfinding made

--- src/Pathfinding.java
+++ src/Pathfinding.java
@@ -2,27 +2,56 @@ import java.util.*;
 public class Pathfinding {
     private Pnode start, finish;
     private Graph graph;
+
     Pathfinding(int s, int f, Graph g) {
         graph = g;
         start = new Pnode(graph.getConnections(s));
         finish = new Pnode(graph.getConnections(f));
     }
-    public int[] findPath() {
+
+    public int findPath() {
         Pnode[] coolerGraph = new Pnode[graph.getSize()];
-        for (int i = 0; i<graph.getSize(); i++) {
+        for (int i = 0; i < graph.getSize(); i++) {
             coolerGraph[i] = new Pnode(graph.getConnections(i));
         }
+        List<Pnode> closed = new ArrayList<Pnode>();
+
+        // your values will be in a matrix w/ index corresponding to node # in graph
+        PriorityQueue<Pnode> frontier = new PriorityQueue<Pnode>();
         start.setDistance(0);
-        List<Pnode> closed = new ArrayList<>();
-        PriorityQueue<Pnode> open = new PriorityQueue<>(Collections.singleton(start));
+        frontier.add(start);
 
-
-        for (int i = 0; i<coolerGraph.length; i++) {
-            if (i != 0) {
-                coolerGraph[i].setDistance(start.getAdjacentNodes()[i]);
+        while (!frontier.isEmpty()) {
+            //pop out the frontier; we are now evaluating this node
+            Pnode curr = frontier.poll();
+// for each node adjacent to our frontier...
+            for (int i : curr.getAdjacentNodes()) {
+                // check if its 'distance' is inf, meaning it has not been set one yet (in relation to first node)
+                if (coolerGraph[i].getDistance() == Integer.MAX_VALUE) {
+                    // we set its new dist to the curr node dist + the dist from curr to it.
+                    coolerGraph[i].setDistance(curr.getAdjacentDistances()[i] + curr.getDistance());
+                    // this node is now our new frontier.
+                    frontier.add(coolerGraph[i]);
+                } else {
+                    if (curr.getDistance() + curr.getAdjacentDistances()[i] < coolerGraph[i].getDistance()) {
+                        coolerGraph[i].setDistance(curr.getDistance() + curr.getAdjacentDistances()[i]);
+//                        frontier.add(coolerGraph[i])
+                    }
+                }
             }
+            // byebye current, you are being reassigned!
         }
-
+        return finish.getDistance();
     }
-
 }
+/* dist from start node to another node
+dist from its neighbors
+A --> B + B --> C = A ---> C
+first val always going to be less
+to get shortest dist: distance to one before it + that to the next node.
+if its less, it changes, otherwise, change.
+run thru entire graph, get to end, then the dist will be set to the shortest one possible
+
+
+
+*/
--- src/Pnode.java
+++ src/Pnode.java
@@ -1,23 +1,51 @@
 import java.util.*;
-public class Pnode {
+public class Pnode implements Comparable<Pnode>{
 
+    //temporary distance value so we know its not been assigned anything yet
     private Integer dist = Integer.MAX_VALUE;
+    //shortest path of node
     private LinkedList<Pnode> shortestPath = new LinkedList<>();
-    private int[] adjacentNodes;
+    //the node # of its adjacent nodes.
+    private ArrayList<Integer> adjacentNodes;
+    //distances to the adjacent nodes.
+    private int[] adjacentDistance;
 
     public int getDistance() {
         return dist;
     }
 
     public Pnode(int[] adj) {
-        adjacentNodes = adj;
+        adjacentDistance = adj;
+        adjacentNodes = new ArrayList<Integer>(adj.length);
+        for (int i=0;i<adj.length;i++){
+            if (i>0){
+                adjacentNodes.add(i);
+            }
+        }
     }
 
-    public int[] getAdjacentNodes() {
+    public ArrayList<Integer> getAdjacentNodes() {
         return adjacentNodes;
     }
+
+    public int[] getAdjacentDistances() {
+        return adjacentDistance;
+    }
+
+    @Override
+    public int compareTo(Pnode node){
+        if (this.dist == node.getDistance()){
+            return 0;
+        }
+        else if (this.dist < node.getDistance()){
+            return -1;
+        }
+        return 1;
+    }
+
     public int setDistance(int newDist) {
         dist = newDist;
+        return dist;
     }
 
 
